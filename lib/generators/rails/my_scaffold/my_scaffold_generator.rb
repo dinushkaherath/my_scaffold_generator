ec5175edabc2f87e758da22fa1a43bc77ee8c1de
dinushkaherath <dinushka.herath@gmail.com>
2024-09-26 00:23:09 -0400

    updated defaults, add fix for turbo_frame _top

--- lib/generators/rails/my_scaffold/my_scaffold_generator.rb
+++ lib/generators/rails/my_scaffold/my_scaffold_generator.rb
@@ -8,9 +8,78 @@ class Rails::MyScaffoldGenerator < Rails::Generators::NamedBase
 
   argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
+  # Modify the existing migration file
+  def inject_defaults_into_migration
+    migration_file_path = Dir[Rails.root.join('db/migrate/*_create_' + plural_table_name + '.rb')].first
+
+    if migration_file_path
+      inject_into_file migration_file_path, after: "create_table :#{plural_table_name} do |t|\n" do
+        default_lines = attributes.map do |attribute|
+          default_value = case attribute.type.to_sym
+                          when :string then "'New #{attribute.name.capitalize}'"
+                          when :text then "'New #{attribute.name.capitalize}'"
+                          when :integer then "0"
+                          when :boolean then "false"
+                          when :datetime then "Time.current" # or DateTime.current
+                          else "nil" # Fallback for other types
+                          end
+
+          "t.#{attribute.type} :#{attribute.name}, default: #{default_value}"
+        end.join("\n")
+
+        <<~RUBY.indent(6)
+
+          # Set default values
+          #{default_lines}
+
+        RUBY
+      end
+    end
+  end
+
   # Override the default Rails method to generate the model
   def create_custom_model
-    template "model.rb.tt", File.join("app/models", class_path, "#{file_name}.rb"), force: true
+    # Define the path to the model file
+    model_file_path = File.join("app/models", "#{file_name}.rb")
+
+    # Inject additional code into the model file
+    inject_into_file model_file_path, after: "class #{class_name} < ApplicationRecord\n" do
+      # Generate validations
+      validations = attributes.map do |attribute|
+        "validates :#{attribute.name}, presence: true"
+      end.join("\n")
+
+      # Generate set_defaults method with type-based defaults
+      set_defaults = attributes.map do |attribute|
+          default_value = case attribute.type.to_sym
+                          when :string then "'New #{attribute.name.capitalize}'"
+                          when :text then "'New #{attribute.name.capitalize}'"
+                          when :integer then "0"
+                          when :boolean then "false"
+                          when :datetime then "Time.current" # or DateTime.current
+                          else "nil" # Fallback for other types
+                          end
+
+        "  self.#{attribute.name} ||= #{default_value}"
+      end.join("\n")
+
+      <<~RUBY.indent(2)
+        #{validations}
+
+        # Broadcast changes for Hotwire Turbo
+        after_create_commit  { broadcast_prepend_to "#{plural_table_name}", partial: "#{plural_table_name}/#{singular_table_name}", locals: { #{singular_table_name}: self }, target: "#{plural_table_name}" }
+        after_update_commit  { broadcast_replace_to "#{plural_table_name}", partial: "#{plural_table_name}/#{singular_table_name}", locals: { #{singular_table_name}: self } }
+        after_destroy_commit { broadcast_remove_to "#{plural_table_name}" }
+
+        after_initialize :set_defaults, unless: :persisted?
+
+        private
+
+        def set_defaults
+        #{set_defaults}
+        end
+      RUBY
+    end
   end
 
   def create_custom_controller
@@ -23,7 +92,7 @@ class Rails::MyScaffoldGenerator < Rails::Generators::NamedBase
       template "#{view}.html.haml", File.join("app/views", controller_file_path, "#{view}.html.haml"), force: true
     end
 
-    template "_partial.html.haml", File.join("app/views", controller_file_path, "_#{singular_name}.html.haml"), force: true
+    template "_partial.html.haml", File.join("app/views", controller_file_path, "_#{singular_table_name}.html.haml"), force: true
   end
 
   # Generate Turbo Stream templates for real-time updates
@@ -36,7 +105,7 @@ class Rails::MyScaffoldGenerator < Rails::Generators::NamedBase
 
   private
 
-    def available_views
-      %w(index edit show new _form)
-    end
+  def available_views
+    %w[index edit show new _form]
+  end
 end
